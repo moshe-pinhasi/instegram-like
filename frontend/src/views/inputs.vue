3cd4460fd91bd24ef96cff5b1bd21c6877fb1e79
Moshe Pinhasi <pi.moshe@gmail.com>
2021-12-24 13:23:26 +0200

    btns - element style

---
+++ frontend/src/views/inputs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="btns">
+
+    <div class="row">
+      <h3>input default</h3>
+      
+    </div>
+  
+    <hr />
+
+
+
+  </div> 
+</template>
+
+<style lang="scss" scoped>
+.inputs {
+  .row {
+    margin: 20px;
+  }
+}
+</style>
